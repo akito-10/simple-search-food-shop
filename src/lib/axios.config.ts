ba910d06443e488c33d0ee37b8b5ce19af321a98
github akito-10 <akt.fukuda@gmail.com>
2022-07-23 20:51:47 +0900

    🔨 axiosのbaseURLを変更

--- src/lib/axios.config.ts
+++ src/lib/axios.config.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://webservice.recruit.co.jp",
+  baseURL: "http://webservice.recruit.co.jp/hotpepper/",
   responseType: "json",
   headers: {
     "Content-Type": "application/json",
