87be5c68e430eb7249a2aa90b80e341705d6328c
github akito-10 <akt.fukuda@gmail.com>
2022-07-23 20:53:35 +0900

    🏷️ ショップAPI

---
+++ src/model/api/getShops.ts
@@ -0,0 +1,14 @@
+import type { ShopApiRow } from "src/model/shop";
+
+export type GetShopsApi = {
+  request: {
+    lat: number;
+    lng: number;
+    range: number;
+    card: number;
+    midnight: number;
+  };
+  response: {
+    shop: ShopApiRow[];
+  };
+};
