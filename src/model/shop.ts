01b3d061757ca83bd0c25d5c82c4d9479e1f3018
github akito-10 <akt.fukuda@gmail.com>
2022-07-23 20:52:53 +0900

    🏷️ shopAPIからのレスポンスの型

---
+++ src/model/shop.ts
@@ -0,0 +1,29 @@
+import type { SnakeToCamel } from "src/type/snake_to_camel.type";
+
+export type ShopApiRow = {
+  id: string;
+  lat: number;
+  lng: number;
+  address: string;
+  catch: string;
+  coupon_urls: {
+    pc: string;
+    mobile: string;
+  };
+  photo: {
+    pc: {
+      l: string;
+      m: string;
+      s: string;
+    };
+    mobile: {
+      l: string;
+      s: string;
+    };
+  };
+  card: string;
+  open: string;
+  midnight: string;
+};
+
+export type ShopRow = SnakeToCamel<ShopApiRow>;
