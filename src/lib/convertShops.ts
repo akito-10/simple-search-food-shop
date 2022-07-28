2d8854f889823cf5af4de28bfde1994928ad0377
github akito-10 <akt.fukuda@gmail.com>
2022-07-28 23:55:07 +0900

    ✨ レスポンスを整形

---
+++ src/lib/convertShops.ts
@@ -0,0 +1,18 @@
+import type { ShopApiRow, ShopRow } from "src/model/shop";
+
+export const convertShops = (shops: ShopApiRow[] | undefined): ShopRow[] => {
+  if (!shops) return [];
+
+  return shops.map((shop) => ({
+    id: shop.id,
+    lat: shop.lat,
+    lng: shop.lng,
+    address: shop.address,
+    catch: shop.catch,
+    couponUrls: shop.coupon_urls,
+    photo: shop.photo,
+    card: shop.card,
+    open: shop.open,
+    midnight: shop.midnight,
+  }));
+};
